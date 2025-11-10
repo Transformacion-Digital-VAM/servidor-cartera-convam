0288a5828f1aec092bd46a1527780a939bf7d61b
Winy <danielamanzanorangel@gmail.com>
2025-11-10 11:18:42 -0600

    cliente

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const client = require('../controllers/cliente.controller')
 const router = express.Router();
+const { registrarCliente } = require('../controllers/cliente.controller');
+const { validarRegistroCliente } = require('../validators/clienteValidator');
+const { validationResult } = require('express-validator');
 
-
-router.post('/register-client', client.registrarCliente);
-
+router.post('/registrar', validarRegistroCliente, (req, res, next) => {
+  // Manejo de errores de validación
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errores: errors.array() });
+  }
+  next();
+}, registrarCliente);
 
 module.exports = router;

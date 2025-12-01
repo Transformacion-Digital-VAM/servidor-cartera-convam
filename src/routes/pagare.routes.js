99bcc9b459116fc3475108d4549b39862c8494c9
Winy <danielamanzanorangel@gmail.com>
2025-12-01 09:40:24 -0600

    generacion de pagare con datos de credito

--- src/routes/pagare.routes.js
+++ src/routes/pagare.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { generarPagare } = require("../controllers/pagare.controller");
 
-router.post("/generar", generarPagare);
+router.get("/:id_credito", generarPagare);
 
 module.exports = router;

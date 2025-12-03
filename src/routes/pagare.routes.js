3a2078c0c066bcb0beb29d3f3141ebf3c9b4df4a
Winy <danielamanzanorangel@gmail.com>
2025-12-03 11:36:06 -0600

    generación de pagaré con inf de aval

--- src/routes/pagare.routes.js
+++ src/routes/pagare.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { generarPagare } = require("../controllers/pagare.controller");
+const { generarPagare, generarHojaControl } = require("../controllers/pagare.controller");
 
 router.get("/:id_credito", generarPagare);
+router.get("/hoja-control/:id_credito", generarHojaControl);
 
 module.exports = router;
